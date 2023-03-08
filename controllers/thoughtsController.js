95b0788b9489ee3d6d2c1f7b4093fe97c8ec8917
unknown <kevwenner@gmail.com>
2023-03-08 12:51:56 -0500

    filled out all controller calls

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -1,25 +1,56 @@
 const {User, Thought, Reaction} = require('../Models');
 
 module.exports = {
-    getThoughts(){
-        
+    getThoughts(req, res){
+        Thought.find()
+            .then((thought) => res.json(thought))
+            .catch((err) => res.status(500).json(err));
     },
-    getSingleThought(){
+    getSingleThought(req, res){
+        Thought.findOne({id: req.params.thoughtId})
+        .then((thought) => 
+            !thought
+                ? res.status(404).json({message: "No thought with specified ID"})
+                : res.json(thought)
+                )
+                .catch((err) => res.status(500).json(err));
+    },
+    newThought(req, res){
+         Thought.create(req.body)
+            .then((thought) => res.json(thought))
+            .catch((err) => res.status(500).json(err));
+    },
+    updateThought(req, res){
+        Thought.findOneAndUpdate(
+            {id: req.params.thoughtId},
+            {$set: req.body},
+            {runValidators: true, new: true}
+        ).then((thought) => 
+            !thought
+                ? res.status(404).json({ message: "No thought with specified ID"})
+                : res.json(thought) 
+        ).catch((err) => res.status(500).json(err))
 
     },
-    newThought(){
-
+    removeThought(req, res){
+         Thought.findOneAndDelete({id: req.params.thoughtId})
+            .then((thought) =>
+            !thought
+                ? res.status(404).json({ message: "No thought with specified ID"})
+                : res.json(thought) 
+            ).catch((err) => res.status(500).jason(err));
     },
-    updateThought(){
-
+    newReaction(req, res){
+         Reaction.create(req.body)
+            .then((reaction) => res.json(reaction))
+            .catch((err) => res.status(500).json(err));
     },
-    removeThought(){
-
-    },
-    newReaction(){
-
-    },
-    removeReaction(){
-
+    removeReaction(req, res){
+        Reaction.findOneAndDelete({id: req.params.reactionId})
+            .then((reaction) =>
+            !reaction
+                ? res.status(404).json({ message: "No reaction with specified ID"})
+                : res.json(reaction) 
+            ).catch((err) => res.status(500).jason(err));
     }
 }
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,25 +1,49 @@
+const { countReset } = require('console');
 const {User, Thought, Reaction} = require('../Models');
 
 module.exports = {
-    getUsers(){
-        
+    getUsers(req, res){
+        User.find()
+            .then((users) => res.json(users))
+            .catch((err) => res.status(500).json(err));
     }, 
-    newUser(){
+    newUser(req, res){
+        User.create(req.body)
+            .then((user) => res.json(user))
+            .catch((err) => res.status(500).json(err));
+    }, 
+    getSingleUser(req, res){
+        User.findOne({id: req.params.userId})
+        .then((user) => 
+            !user
+                ? res.status(404).json({message: "No user with specified ID"})
+                : res.json(user)
+                )
+                .catch((err) => res.status(500).json(err));
+    }, 
+    updateUser(req, res){
+        User.findOneAndUpdate(
+            {id: req.params.userId},
+            {$set: req.body},
+            {runValidators: true, new: true}
+        ).then((user) => 
+            !user
+                ? res.status(404).json({ message: "No user with specified ID"})
+                : res.json(user) 
+        ).catch((err) => res.status(500).json(err))
+    }, 
+    removeUser(req, res){
+        User.findOneAndDelete({id: req.params.userId})
+            .then((user) =>
+            !user
+                ? res.status(404).json({ message: "No user with specified ID"})
+                : res.json(user) 
+            ).catch((err) => res.status(500).jason(err));
+    }, 
+    addFriend(req, res){
 
     }, 
-    getSingleUser(){
-
-    }, 
-    updateUser(){
-
-    }, 
-    removeUser(){
-
-    }, 
-    addFriend(){
-
-    }, 
-    removeFriend(){
+    removeFriend(req, res){
 
     }
 }
